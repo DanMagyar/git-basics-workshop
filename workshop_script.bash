7dd647fdf5db21f8d5fc52bb06d6ef883b23707e
Daniel Magyar <daniel.laszlo.magyar@gmail.com>
2019-03-05 23:24:10 +0100

    Add logging options and some more comments

--- workshop_script.bash
+++ workshop_script.bash
@@ -1,13 +1,17 @@
 #!/usr/bin/bash
 GIT_DIR="${HOME}/workshops/bme-git-repo-test"
 
-#create directory and make it a git repo
+
+#####################   PART 1 - Repom Working directory, HEAD, stage/add, diff, commit, log  #####################
+#every directory can be a git repository
 mkdir $GIT_DIR --parent
 cd $GIT_DIR || exit 1
 git init
 
-#Create first commit
+# Working tree: the currently checked out snapshot of the git repo
 git status #there is no changes in this directory
+
+#Create first commit
 cat <<EOF >text_file.txt
 Hello Git World!
 What a fine day to learn about git!
@@ -17,10 +21,18 @@ git status #there is a modified file
 git diff #there is the added line
 git add text_file.txt
 git status #there is a file that will be committed
-git commit --message="Say Hello to the world of git!"
+git commit --message='Say Hello to the world of git!'
 git status #there is no changes here
 
-#second commit
+#Do a trivial second commit
 cat <<EOF >text_file.txt
 Good day Git World!
+What a fine day to learn about git!
 EOF
+git commit --all --message='Make greeting more polite'
+
+#Check the revisions
+git log #HEAD: working tree, master: default branch referenced by HEAD
+git log --graph #draw a graph made of commit parent relations
+git log --graph --oneline # make it short
+#optional fancy stuff alias prettylog='git log --full-history --all --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit'
