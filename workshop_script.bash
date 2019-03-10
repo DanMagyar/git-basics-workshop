7f0c1e7f6b1cca1cf409921e9aa1dd73e7d8b0a5
Daniel Magyar <daniel.laszlo.magyar@gmail.com>
2019-03-10 21:47:36 +0100

    Add branch example

--- workshop_script.bash
+++ workshop_script.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 GIT_DIR="${HOME}/workshops/bme-git-repo-test"
 
+#TODO: git config
 
-#####################   PART 1 - Repom Working directory, HEAD, stage/add, diff, commit, log  #####################
+#####################   PART 1 - Repo, Working directory, HEAD, stage/add, diff, commit, log  #####################
 #every directory can be a git repository
 mkdir $GIT_DIR --parent
 cd $GIT_DIR || exit 1
@@ -39,13 +40,25 @@ git log --graph --oneline # make it short
 
 
 
+#####################   PART 2 - branches  #####################
+git checkout -b random_xkcd
+git log # HEAD now points to new branch
+cat <<EOF >random_xkcd.txt
+- Make me a sandwich.
+- No.
+- SUDO make me a sandwhich.
+- Ok.
+EOF
+git stage random_xkcd.txt
+git commit -m "Describe real life sandwhich making"
 
 
 
+#TODO: tags
 
 
 #####################   OPTIONAL path - detached HEAD state #####################
-git checkout $(git rev-parse HEAD)``
+git checkout $(git rev-parse HEAD)
 cat <<EOF >text_file.txt
 This change won't be seen on any of the branches until we check it out explicitly.
 EOF
